5debab26d51c02de6bab514f0c1a36b5d99c7636
gabriel.vicente <gabrielvf64@gmail.com>
2025-02-09 16:39:30 -0300

    build: populate user table on startup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,10 @@ VALUES
 ('Clean Code', 'Robert Martin', 'Prentice Hall', '9780132350884', 'AVAILABLE'),
 ('Effective Java', 'Joshua Bloch', 'Addison-Wesley', '9780134685991', 'AVAILABLE'),
 ('Design Patterns', 'Erich Gamma', 'Addison-Wesley', '9780201633610', 'BORROWED');
+
+INSERT INTO users (name, cpf)
+VALUES ('Eduardo', '111.222.333-44'),
+       ('Elyonay', '444.555.666-77'),
+       ('Erick', '555.666.777-88'),
+       ('Gabriel', '123.456.789-00'),
+       ('Giovanni', '987.654.321-00');
