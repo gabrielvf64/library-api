53eb92ebc234ddc4e83e45e8a6acf9b328b5c873
Eduardo Rodrigues <eduardodsr@gmail.com>
2025-02-10 17:58:13 -0300

    Create Class LibraryUser

--- src/main/java/com/box/library/user/LibraryUser.java
+++ src/main/java/com/box/library/user/LibraryUser.java
@@ -1,4 +1,48 @@
 package com.box.library.user;
 
+
+import jakarta.persistence.*;
+
+@Table(name = "users")
 public class LibraryUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cpf;
+    private String name;
+
+    public LibraryUser() {
+    }
+
+    public LibraryUser(Long id, String cpf, String name) {
+        this.id = id;
+        this.cpf = cpf;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
