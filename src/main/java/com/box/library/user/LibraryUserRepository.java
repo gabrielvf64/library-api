b96bbc2d645fe4f0c919f40e462de96d464d3723
gabriel.vicente <gabrielvf64@gmail.com>
2025-02-11 07:50:48 -0300

    annotation for possible future custom methods

--- src/main/java/com/box/library/user/LibraryUserRepository.java
+++ src/main/java/com/box/library/user/LibraryUserRepository.java
@@ -1,6 +1,8 @@
 package com.box.library.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LibraryUserRepository extends JpaRepository<LibraryUser, Long> {
 }
