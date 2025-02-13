c7a58b5465886194af922703ceea3d9fd3098664
Eduardo Rodrigues <eduardodsr@gmail.com>
2025-02-12 22:27:52 -0300

    ✨ feat: Create LibraryUserRepositor

---
+++ src/main/java/com/box/library/user/LibraryUserRepository.java
@@ -0,0 +1,7 @@
+package com.box.library.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LibraryUserRepository extends JpaRepository<LibraryUser, Long> {
+
+}
