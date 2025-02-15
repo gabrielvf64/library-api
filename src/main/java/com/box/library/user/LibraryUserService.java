6579a97982a8a1bbd2168824fc347c22924da792
gabriel.vicente <gabrielvf64@gmail.com>
2025-02-14 22:49:48 -0300

    feat: delete user by id

--- src/main/java/com/box/library/user/LibraryUserService.java
+++ src/main/java/com/box/library/user/LibraryUserService.java
@@ -1,5 +1,6 @@
 package com.box.library.user;
 
+import com.box.library.exception.UserNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,5 +18,18 @@ public class LibraryUserService {
         return repository.save(user);
     }
 
-    public List<LibraryUser> findAll() { return repository.findAll(); }
+    public List<LibraryUser> findAll() {
+        return repository.findAll();
+    }
+
+    public void deleteById(Long id) {
+        if (doesNotExitsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        repository.deleteById(id);
+    }
+
+    private boolean doesNotExitsById(Long id) {
+        return !repository.existsById(id);
+    }
 }
