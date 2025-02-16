b17afbf9b62e91bdc1d7a90085b7e970004290c0
gabriel.vicente <gabrielvf64@gmail.com>
2025-02-16 08:36:32 -0300

    report endpoint using html response

--- src/main/java/com/box/library/loan/LoanController.java
+++ src/main/java/com/box/library/loan/LoanController.java
@@ -2,6 +2,7 @@ package com.box.library.loan;
 
 import com.box.library.request.CreateLoan;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,5 +29,13 @@ public class LoanController {
     public ResponseEntity<Loan> create(@RequestBody CreateLoan request) {
         Loan savedLoan = service.create(request);
         return ResponseEntity.ok(savedLoan);
+    }
+
+    @GetMapping("/report")
+    public ResponseEntity<String> getLoanReport(@RequestParam("status") LoanStatus status) {
+        String htmlReport = service.generateLoanReport(status);
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .body(htmlReport);
     }
 }
