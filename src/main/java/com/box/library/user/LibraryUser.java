ea3e65d332f6d176f1841d15db1c9b5639cc9f47
gabriel.vicente <gabrielvf64@gmail.com>
2025-02-28 22:39:41 -0300

    refactor: add new LibraryUser attributes

--- src/main/java/com/box/library/user/LibraryUser.java
+++ src/main/java/com/box/library/user/LibraryUser.java
@@ -9,14 +9,28 @@ public class LibraryUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String username;
+
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     private String cpf;
+
     private String name;
 
     public LibraryUser() {
     }
 
-    public LibraryUser(Long id, String cpf, String name) {
+    public LibraryUser(Long id, String username, String password,
+                       Role role, String cpf, String name) {
         this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
         this.cpf = cpf;
         this.name = name;
     }
@@ -27,6 +41,30 @@ public class LibraryUser {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     public String getCpf() {
