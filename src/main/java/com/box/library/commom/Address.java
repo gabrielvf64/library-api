6612344fe7b12529ea8b486cb5625c0f234f045f
Eduardo Rodrigues <eduardodsr@gmail.com>
2025-03-02 00:10:42 -0300

    feat: add Address class and integrate with LibraryUser entity

---
+++ src/main/java/com/box/library/commom/Address.java
@@ -0,0 +1,18 @@
+package com.box.library.commom;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+public class Address {
+
+    private String city;
+    private String street;
+    private String number;
+    private String complement;
+    private String zipCode;
+
+}
--- src/main/java/com/box/library/user/LibraryUser.java
+++ src/main/java/com/box/library/user/LibraryUser.java
@@ -1,16 +1,25 @@
 package com.box.library.user;
 
+import com.box.library.commom.Address;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class LibraryUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String cpf;
     private String name;
+
+    @Embedded
+    private Address address;
 
     public LibraryUser() {
     }
@@ -18,30 +27,6 @@ public class LibraryUser {
     public LibraryUser(Long id, String cpf, String name) {
         this.id = id;
         this.cpf = cpf;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 }
