92fb07a613df2bd670a9cdcfd4682dd93cb07cda
Eduardo Rodrigues <eduardodsr@gmail.com>
2025-03-03 00:25:10 -0300

    refactor: rename Address class package and update LibraryUser to use new package structure

--- src/main/java/com/box/library/customer/Address.java
+++ src/main/java/com/box/library/customer/Address.java
@@ -1,13 +1,19 @@
-package com.box.library.commom;
+package com.box.library.customer;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Embeddable
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
+
 
     private String city;
     private String street;
--- src/main/java/com/box/library/user/LibraryUser.java
+++ src/main/java/com/box/library/user/LibraryUser.java
@@ -1,33 +1,26 @@
 package com.box.library.user;
 
-import com.box.library.commom.Address;
+import com.box.library.customer.Address;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
+@NoArgsConstructor
 public class LibraryUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String cpf;
     private String name;
 
     @Embedded
     private Address address;
 
-    public LibraryUser() {
-    }
-
-    public LibraryUser(Long id, String cpf, String name) {
-        this.id = id;
-        this.cpf = cpf;
-        this.name = name;
-    }
 }
 
