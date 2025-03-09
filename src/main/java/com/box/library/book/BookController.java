ffa82c5b0c91c016cc9533a0c48b9279a9d7efb5
Eduardo Rodrigues <eduardodsr@gmail.com>
2025-03-09 17:08:24 -0300

    feat: add security annotations to BookController methods for role-based access control

--- src/main/java/com/box/library/book/BookController.java
+++ src/main/java/com/box/library/book/BookController.java
@@ -4,6 +4,7 @@ import com.box.library.request.UpdateBook;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,49 +14,49 @@ import java.util.List;
 @Tag(name = "Livros", description = "Endpoints para gerenciamento de livros")
 public class BookController {
 
-    // TODO[5]: CRUD de livros
-
     private final BookService service;
 
     public BookController(BookService service) {
         this.service = service;
     }
 
-    // TODO[1]: Criação de livro
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Book> create(@RequestBody Book book) {
-
-        // TODO[9]: Usando var
         var savedBook = service.create(book);
         return ResponseEntity.ok(savedBook);
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'CLIENT')")
     public ResponseEntity<List<Book>> findAll() {
         var books = service.findAll();
         return books.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(books);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'CLIENT')")
     public ResponseEntity<Book> findById(@PathVariable Long id) {
         var book = service.findById(id);
         return ResponseEntity.ok(book);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Book> update(@PathVariable Long id, @RequestBody UpdateBook request) {
         var updatedEntity = service.update(id, request);
         return new ResponseEntity<>(updatedEntity, HttpStatus.OK);
     }
 
-    // TODO[4]: Busca de livros por filtros
     @GetMapping("/filter")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'CLIENT')")
     public ResponseEntity<List<Book>> findAllByFilter(@RequestParam(required = false) String author, @RequestParam(required = false) String title,
                                                       @RequestParam(required = false) String isbn, @RequestParam(required = false) String publisher) {
         var books = service.findAllByFilter(author, title, isbn, publisher);
