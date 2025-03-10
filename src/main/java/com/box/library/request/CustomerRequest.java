406aed92f787d64f2480176de7c1e95490fe051b
gabriel.vicente <gabrielvf64@gmail.com>
2025-03-10 20:51:00 -0300

    refactor: separate create and update customer dto

--- src/main/java/com/box/library/request/CustomerRequest.java
+++
@@ -1,8 +0,0 @@
-package com.box.library.request;
-
-import jakarta.validation.constraints.NotBlank;
-import org.hibernate.validator.constraints.br.CPF;
-
-public record CustomerRequest(@NotBlank String name,
-                              @NotBlank @CPF(message = "CPF inválido") String cpf) {
-}
---
+++ src/main/java/com/box/library/request/UpdateCustomerRequest.java
@@ -0,0 +1,8 @@
+package com.box.library.request;
+
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record UpdateCustomerRequest(@NotBlank String name,
+                                    @NotBlank @CPF(message = "CPF inválido") String cpf) {
+}
