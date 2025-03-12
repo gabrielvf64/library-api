6aef65399666dabf4ac54874980bf1a4783019f0
Giovanni Duarte <garduarte94@gmail.com>
2025-03-12 08:27:29 -0300

    refactor: correcting pending loan exception message

--- src/main/java/com/box/library/exception/PendingLoanException.java
+++ src/main/java/com/box/library/exception/PendingLoanException.java
@@ -2,6 +2,6 @@ package com.box.library.exception;
 
 public class PendingLoanException extends RuntimeException {
     public PendingLoanException(Long customerId) {
-        super("User with id " + customerId + " has pending loan.");
+        super("Customer with id " + customerId + " has pending loan.");
     }
 }
